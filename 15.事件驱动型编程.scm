8eeca4b60c2faeaaec68b1def8a0afbace375f05
rzdtjb <rzdtjb@gmail.com>
2022-01-28 19:17:36 +0800

    update to 15

--- 15.事件驱动型编程.scm
+++ 15.事件驱动型编程.scm
@@ -1,5 +1,4 @@
-;;
-
+;;logic gate
 (define (inverter input output)
   (define (invert-input)
     (let ((new-value (logical-not (get-signal input))))
@@ -32,10 +31,7 @@
     (* a b)
     (error "Invalid signal" (list a b))))
 
-
-
-;
-
+;;
 (define (or-gate a1 a2 output)
   (define (or-action-procedure)
     (let ((new-value
@@ -54,6 +50,40 @@
     (min 1 (+ a b))
     (error "Invalid signal" (list a b))))
 
+;;wire object
+(define (make-wire)
+  (let ((signal-value 0)
+        (action-procedures '()))
+    (define (set-my-signal! new-value)
+      (if (not (= signal-value new-value))
+          (begin
+            (set! signal-value new-value)
+            (call-each action-procedures))
+          'done))
+    (define (accept-action-procedure! proc)
+      (set! action-procedures (cons proc action-procedures))
+      (proc))
+    (define (dispatch m)
+      (cond ((eq? m 'get-signal) signal-value)
+            ((eq? m 'set-signal!) set-my-signal!)
+            ((eq? m 'add-action!) accept-action-procedure!)
+            (else
+             (error "Unknown operation -- WIRE " m))))
+    dispatch))
 
+(define (call-each procedures)
+  (if (null? procedures)
+      'done
+      (begin
+        ((car procedures))
+        (call-each (cdr procedures)))))
 
+(define (get-signal wire)
+  (wire 'get-signal))
+
+(define (set-signal! wire)
+  ((wire 'set-signal!) new-value))
+
+(define (add-action! wire action-procedure)
+  ((wire 'add-action!) action-procedure))
 
