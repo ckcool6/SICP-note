ad76d655655c553142564363218dc08af497b38e
rzdtjb <rzdtjb@gmail.com>
2022-01-29 15:23:27 +0800

    update to 15

---
+++ 11.表格结构.scm
@@ -0,0 +1,113 @@
+;; use chez scheme
+;; one-d table
+
+(define (lookup key table)
+  (let ((record (assoc key (cdr table))))
+    (if record
+	(cdr record)
+	#f)))
+
+(define (assoc key records)
+  (cond ((null? records)
+	 #f)
+	((equal? key (caar records))
+	 (car records))
+	(else
+	 (assoc key (cdr records)))))
+
+(define (insert! key value table)
+  (let ((record (assoc key (cdr table))))
+    (if record
+	(set-cdr! record value)
+	(set-cdr! table
+		  (cons
+		   (cons key value)
+		   (cdr table)))))
+  'insert-OK)
+
+(define (make-table)
+  (list '*table*))
+
+;; test
+
+(define sheet (make-table))
+
+(insert! 'a 1 sheet)
+(insert! 'b 2 sheet)
+(insert! 'c 3 sheet)
+(insert! 'd 4 sheet)
+
+sheet
+
+(lookup 'd sheet)
+(lookup 'a sheet)
+
+;; two-d table
+
+(define (lookup-2 key-1 key-2 table)
+  (let ((subtable (assoc key-1 (cdr table))))
+    (if subtable
+	 (let ((record (assoc key-2 (cdr subtable))))
+	   (if record
+	       (cdr record)
+	       #f))
+	 #f)))
+
+(define (insert-! key-1 key-2 value table)
+  (let ((subtable (assoc key-1 (cdr table))))
+    (if subtable
+	(let ((record (assoc key-2 (cdr subtable))))
+	  (if record
+	      (set-cdr! record value)
+	      (set-cdr! subtable
+			(cons (cons key-2 value)
+			      (cdr subtable)))))
+	(set-cdr! table
+		  (cons (list key-1
+			      (cons key-2 value))
+			(cdr table)))))
+  'OK)
+
+;; create local table
+
+(define (make-table)
+  (let ((local-table (list '*table*)))
+    (define (lookup-2 key-1 key-2)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+	(if subtable
+	    (let ((record (assoc key-2 (cdr subtable))))
+	      (if record
+		  (cdr record)
+		  #f))
+	    #f)))
+    (define (insert-! key-1 key-2 value)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+	(if subtable
+	    (let ((record (assoc key-2 (cdr subtable))))
+	      (if record
+		  (set-cdr! record value)
+		  (set-cdr! subtable
+			    (cons (cons key-2 value)
+				  (cdr subtable)))))
+	    (set-cdr! local-table
+		      (cons (list key-1
+				  (cons key-2 value))
+			    (cdr local-table)))))
+      'OK)
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup-2)
+	    ((eq? m 'insert-proc!) insert-!)
+	    (else
+	     (error "Unknow operation -- TABLE" m))))
+    dispatch))
+
+;; put and get
+
+(define operation-table (make-table))
+(define get (operation-table 'lookup-proc))
+(define put (operation-table 'insert-proc!))
+
+;;test
+(put 'math '+ 4)
+(put 'math '- 7)
+(get 'math '+)
--- 15.事件驱动型编程.scm
+++ 15.事件驱动型编程.scm
@@ -81,9 +81,192 @@
 (define (get-signal wire)
   (wire 'get-signal))
 
-(define (set-signal! wire)
+(define (set-signal! wire new-value)
   ((wire 'set-signal!) new-value))
 
 (define (add-action! wire action-procedure)
   ((wire 'add-action!) action-procedure))
 
+(define (after-delay delay action)
+  (add-to-agenda! (+ delay (current-time the-agenda))
+                  action
+                  the-agenda))
+
+(define (propagate)
+  (if (empty-agenda? the-agenda)
+      'done
+      (let ((first-item (first-agenda-item the-agenda)))
+        (first-item)
+        (remove-first-agenda-item! the-agenda)
+        (propagate))))
+
+;;structure of agenda:
+(define (make-time-segment time queue)
+  (cons time queue))
+
+(define (segment-time s)
+  (car s))
+
+(define (segment-queue s)
+  (cdr s))
+
+(define (make-agenda)
+  (list 0))
+
+(define (current-time agenda)
+  (car agenda))
+
+(define (set-current-time! agenda time)
+  (set-car! agenda segments))
+
+(define (segments agenda)
+  (cdr agenda))
+
+(define (set-segments! agenda segments)
+  (set-cdr! agenda segments))
+
+(define (first-segment agenda)
+  (car (segments agenda)))
+
+(define (rest-sements agenda)
+  (cdr (segments agenda)))
+
+(define (empty-agenda? agenda)
+  (null? (segments agenda)))
+
+(define (add-to-agenda! time action agenda)
+  (define (belongs-before? segments)
+    (or (null? segments)
+        (< time (segment-time (car segments)))))
+  (define (make-new-time-segment time action)
+    (let ((q (make-queue)))
+      (insert-queue! q action)
+      (make-time-segment time q)))
+  (define (add-to-segments! segments)
+    (if (= (segment-time (car segments)) time)
+        (insert-queue! (segment-queue (car segments))
+                       action)
+        (let ((rest (cdr segments)))
+          (if (belongs-before? rest)
+              (set-cdr!
+               segments
+               (cons (make-new-time-segment time action)
+                     (cdr segments)))
+              (add-to-segments! rest)))))
+  (let ((segments (segments agenda)))
+    (if (belongs-before? segments)
+        (set-segments!
+         agenda
+         (cons (make-new-time-segment time action)
+               segments))
+        (add-to-segments! segments))))
+
+(define (remove-first-agenda-item! agenda)
+  (let ((q (segment-queue (first-segment agenda))))
+    (delete-queue! q)
+    (if (empty-queue? q)
+        (set-segments! agenda (rest-segments agenda)))))
+
+(define (first-agenda-item agenda)
+  (if (empty-agenda? agenda)
+      (error "Agenda is empty")
+      (let ((first-seg (first-segment agenda)))
+        (set-current-time! agenda (segment-time first-seg))
+        (front-queue (segment-queue first-seg)))))
+
+(define front-pointer
+  (lambda (queue)
+    (car queue)))
+
+(define rear-pointer
+  (lambda (queue)
+    (cdr queue)))
+
+(define set-front-ptr!
+  (lambda (queue item)
+    (set-car! queue item)))
+
+(define set-rear-ptr!
+  (lambda (queue item)
+    (set-cdr! queue item)))
+
+(define empty-queue?
+  (lambda (queue)
+    (null? (front-pointer queue))))
+
+(define make-queue
+  (lambda () (cons '() '())))
+
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "FRONT called with an empty queue" queue)
+      (car (front-pointer queue))))
+
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue)
+	   (set-front-ptr! queue new-pair)
+	   (set-rear-ptr! queue new-pair)
+	   queue)
+	  (else
+	   (set-cdr! (rear-pointer queue) new-pair)
+	   (set-rear-ptr! queue new-pair)
+	   queue))))
+
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue)
+	 (error "DELETE called with an empty queue" queue))
+	(else
+	 (set-front-ptr! queue (cdr
+				(front-pointer queue)))
+	 queue)))
+
+;;test
+(define (half-adder a b s c)
+  (let ((d (make-wire))
+        (e (make-wire)))
+    (or-gate a b d)
+    (and-gate a b c)
+    (inverter c e)
+    (and-gate d e s)
+    'OK))
+
+(define (probe name wire)
+  (add-action! wire
+               (lambda ()
+                 (newline)
+                 (display name)
+                 (display " ")
+                 (display (current-time the-agenda))
+                 (display " New-value = ")
+                 (display (get-signal wire)))))
+
+;;init
+(define the-agenda (make-agenda))
+the-agenda
+(define inverter-delay 2)
+(define and-gate-delay 3)
+(define or-gate-delay 5)
+
+;;
+(define input-1 (make-wire))
+(define input-2 (make-wire))
+(define sum (make-wire))
+(define carry (make-wire))
+
+(probe 'sum sum)
+(probe 'carry carry)
+;;output result
+;; 1 ]=> 
+;; sum 0 New-value = 0
+
+;; 1 ]=> 
+;; carry 0 New-value = 0
+
+(half-adder input-1 input-2 sum carry)
+;; vlaue: ok
+
+(set-signal! input-1 1)
+;; value: done
+(set-signal! input-2 1)
+(propagate)
